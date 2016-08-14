5d681ada1b0e49721c246f05fe7e9322c90dc725
partokarwat <pk@fanrir.com>
2016-08-14 20:53:30 +0200

    chg: turtle drawing a flower with output

--- mindstorms.py
+++ mindstorms.py
@@ -1,24 +1,41 @@
 import turtle
 
 def draw_square(some_turtle):
-    for j in range (0,4):
+    for i in range (0,4):
         some_turtle.forward(100)
         some_turtle.right(90)
 
+def draw_triangle(some_turtle):
+    for i in range (0,3):
+        some_turtle.forward(100)
+        some_turtle.right(120)
 
+def draw_diamond(some_turtle):
+    some_turtle.left(45)
+    some_turtle.forward(100)
+    some_turtle.right(45)
+    some_turtle.forward(100)
+    some_turtle.right(135)
+    some_turtle.forward(100)
+    some_turtle.right(45)
+    some_turtle.forward(100) 
 
 def draw_art():
     window = turtle.Screen()
-    window.bgcolor("red")
+    window.bgcolor("white")
     
     brad = turtle.Turtle()
-    brad.shape("circle")
-    brad.color("yellow")
+    brad.shape("turtle")
+    brad.color("blue")
     brad.speed(10)
 
     for i in range (0,36):
-        draw_square(brad)
-        brad.left(10)
+        draw_diamond(brad)
+        brad.right(10)
+
+    brad.right(90)
+    brad.forward(300)
+    
     window.exitonclick()
     
     
