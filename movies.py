f3249e7cef82e270f41b4caa0cc7b9f65a93a841
partokarwat <pk@fanrir.com>
2016-08-18 19:30:15 +0200

    add: Mini-Project Movie Website

---
+++ movies.py
@@ -0,0 +1,13 @@
+import media
+import fresh_tomatoes
+
+avatar = media.Movie("Avatar", "A marine on an alien planet",
+                     "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_.jpg",
+                     "https://www.youtube.com/watch?v=EzETGqZN6dU")
+
+ratatouille = media.Movie("Ratatouille", "A rat is a chef in Paris",
+                     "http://vignette3.wikia.nocookie.net/moviepedia/images/2/29/Poster-de-ratatouille.jpg/revision/latest?cb=20130930090447&path-prefix=de",
+                     "https://www.youtube.com/watch?v=wlCAq45TcxU")
+
+movies = [avatar, ratatouille]
+fresh_tomatoes.open_movies_page(movies)
