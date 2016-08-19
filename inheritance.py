41a7eae79b968abdb706491c3aaa724e8b4e7e20
partokarwat <pk@fanrir.com>
2016-08-19 12:17:45 +0200

    add: script showing inheritance an method overriding in python

---
+++ inheritance.py
@@ -0,0 +1,27 @@
+class Parent():
+    def __init__(self, last_name, eye_color):
+        print("Parent constructor called")
+        self.last_name = last_name
+        self.eye_color = eye_color
+
+    def show_info(self):
+        print("Last Name - "+self.last_name)
+        print("Eye Color - "+self.eye_color)
+
+class Child(Parent):
+    def __init__(self, last_name, eye_color, number_of_toys):
+        print("Child constructur called")
+        Parent.__init__(self, last_name, eye_color)
+        self.number_of_toys = number_of_toys
+
+    def show_info(self):
+        print("Last Name - "+self.last_name)
+        print("Eye Color - "+self.eye_color)
+        print("Number of toys - "+str(self.number_of_toys))
+    
+mom = Parent("Miller", "yellow")
+# mom.show_info()
+
+lisa = Child("Miller", "yellow", 5)
+lisa.show_info()
+
