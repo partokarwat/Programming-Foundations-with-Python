8b8b4ebf8045c9020341c3491da491edfec41bf6
partokarwat <pk@fanrir.com>
2016-08-19 12:34:44 +0200

    add: created parent class Video for Movie and new child class TvShow (inheritance example)

--- media.py
+++ media.py
@@ -1,11 +1,27 @@
 import webbrowser
 
-class Movie():
+class Video():
+    def __init__(self, title, duration):
+        self.title = title
+        self.duration = duration
+
+class TvShow(Video):
+    def __init__(self, tv_show_title, tv_show_season, tv_show_episode,
+                 tv_show_tv_station):
+        Video.__init__(self, tv_show_title, 0)
+        self.tv_show_season = tv_show_season
+        self.tv_show_episode = tv_show_episode
+        self.tv_show_tv_station = tv_show_tv_station
+
+    def get_local_listing(self):
+        print("Something")    
+
+class Movie(Video):
     """ This class provides a way to store moview related information"""
     
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
-        self.title = movie_title
+        Video.__init__(self, movie_title, 0)
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
